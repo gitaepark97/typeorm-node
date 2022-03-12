77038a2079f2b393a0426317ea484ded942e28e4
gitaepark97 <parkkitae7@gmail.com>
2022-03-12 12:56:51 +0900

    create data

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { createConnection } from 'typeorm'
+import express from 'express'
 import { Banker } from './entities/Banker'
 import { Client } from './entities/Client'
 import { Transaction } from './entities/Transaction'
+import { createClientRouter } from './routes/create_client'
+import { createBankerRouter } from './routes/create_banker'
+
+const app = express()
 
 const main = async () => {
   try {
@@ -16,6 +21,14 @@ const main = async () => {
       synchronize: true,
     })
     console.log('Connected to MariaDB')
+
+    app.use(express.json())
+    app.use(createClientRouter)
+    app.use(createBankerRouter)
+
+    app.listen(8000, () => {
+      console.log('Now running on port 8000')
+    })
   } catch (error) {
     console.error(error)
     throw new Error('Unable to connect to DB')
